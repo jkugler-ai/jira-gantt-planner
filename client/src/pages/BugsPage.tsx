882a5fb173ed1dd333a3357904f905b9461ea9f1
Horde <horde@nvidia.com>
2026-05-27 19:55:09 +0000

    fix: bugs page default JQL scoped to Storage team dev teams

--- client/src/pages/BugsPage.tsx
+++ client/src/pages/BugsPage.tsx
@@ -6,7 +6,7 @@ export default function BugsPage() {
       pageId="bugs"
       title="Bugs"
       subtitle="Track open bugs and defects"
-      defaultJql='project = OMPE AND issuetype = Bug AND statusCategory != Done ORDER BY priority ASC, created DESC'
+      defaultJql='project = OMPE AND issuetype = Bug AND statusCategory != Done AND "Development Team" in ("Storage Infrastructure APIs", "USD Storage API", "Caching Services", Portal, ovstorage, ovpackage) ORDER BY priority ASC, created DESC'
       extraColumns={['priority', 'fixVersion', 'created', 'nvbugs', 'reporter']}
       showStatusFilter={true}
       hideProductManagerFilter={true}
